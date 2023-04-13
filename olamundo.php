4e930baae5640be8bfaf981376b0ab22d829e8a2
Gabriel Centeno <bielcenteno7@gmail.com>
2023-04-13 11:51:41 -0300

    commited exercises

---
+++ olamundo.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Primeiro exercício em PHP</title>
+  <link rel="stylesheet" href="./_css/estilo.css">
+  <style>
+    h2 {
+      color: blue;
+      text-shadow: 1px 1px 1px black;
+    }
+  </style>
+</head>
+<body>
+  <?php 
+    echo "<h2>Olá, mundo</h2>";
+  ?>
+  
+</body>
+</html>
