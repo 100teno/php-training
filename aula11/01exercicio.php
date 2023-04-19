5c1f205b91ee3ae1971980124ea6a9ef225090c4
Gabriel Centeno <bielcenteno7@gmail.com>
2023-04-19 11:42:28 -0300

    y

---
+++ aula11/01exercicio.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Curso de PHP</title>
+  <link rel="stylesheet" href="../_css/estilo.css">
+</head>
+<body>
+  <div>
+    <?php
+      $c = 10;
+      while ($c>=1) {
+        echo $c . "<br>";
+        $c--;
+      }
+    ?>
+    <a href="javascript:history.go(-1)" class="botao" >Voltar</a>
+    
+    
+  </div>
+</body>
+</html> 
