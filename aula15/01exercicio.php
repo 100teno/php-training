44e9353e548109a40c51ea06bfc0fbdbd0e19199
Gabriel Centeno <bielcenteno7@gmail.com>
2023-04-26 11:04:50 -0300

    a

---
+++ aula15/01exercicio.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Curso de PHP</title>
+  <link rel="stylesheet" href="../_css/estilo.css">
+</head>
+<body>
+  <div>
+    <?php
+      function teste(&$x) {
+        $x += 2;
+        echo "<p>O valor de X é $x</p>";
+
+      }
+
+      $a = 3;
+      teste($a);
+      echo "<p>O valor de A é $a</p>"
+       
+
+     
+    ?>
+    
+    
+    
+  </div>
+</body>
+</html> 
---
+++ aula15/02exercicio.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Curso de PHP</title>
+  <link rel="stylesheet" href="../_css/estilo.css">
+</head>
+<body>
+  <div>
+    <?php
+      include "funcoes2.php";
+      echo "<h1>Testando novas funções</h1>";
+      ola();
+      mostraValor(4);
+      echo "<h1>Finalizando programa...</h1>" 
+
+     
+    ?>
+    
+    
+    
+  </div>
+</body>
+</html> 
